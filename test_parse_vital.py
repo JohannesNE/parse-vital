6e6ff2862e086dc430421dd9efa55b28b1d994b9
JohannesNE <johannes.ne@gmail.com>
2019-08-12 11:30:36 +0200

    update test file

--- test_parse_vital.py
+++ test_parse_vital.py
@@ -1,10 +1,24 @@
 from parse_vital import Vital
+import matplotlib
 
+test_path = "test/test_intellivue_demo1.vital"
 
-ipath = "/home/johannes/Dropbox/Docs/PhD/general-data-analysis/test-data/test.vital"
+test_file = Vital(test_path)
+print(test_file)
 
-test_file = Vital(ipath)
-print(test_file.file.header)
-print(test_file.file.body[1])
+test_track1 = test_file.get_track(name="PLETH_SAT_O2")
+test_track2 = test_file.get_track(name="ABP")
+
+print(test_track1)
+
+# Save track to file
+#test_track2.save_to_file()
+
+# Convert track to Pandas Time series
+test_track2_ts = test_track2.to_pandas_ts()
+
+test_track2_ts.plot()
+matplotlib.pyplot.show()
+
 
 
